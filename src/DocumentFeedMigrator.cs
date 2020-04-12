5d4376f7306c565fbc2217a8b6ab3f522ce30215
TheovanKraay <thvankra@microsoft.com>
2020-04-13 00:04:59 +0100

    clean up partition key mapping feature

--- src/DocumentFeedMigrator.cs
+++ src/DocumentFeedMigrator.cs
@@ -15,21 +15,14 @@ namespace MigrationExecutorFunctionApp
     using System.Collections.Generic;
     using System.Diagnostics;
     using System.Linq;
-    using System.Runtime.Serialization.Json;
-    using System.Text;
     using System.Threading;
     using System.Threading.Tasks;
-    using System.Xml.Linq;
-    using System.Xml.XPath;
 
     public class DocumentFeedMigrator
     {
         private Container containerToStoreDocuments;
-        private string pkname = "pk1";
         string targetPartitionKeyAttribute = Environment.GetEnvironmentVariable($"{"TargetPartitionKeyAttribute"}");
         static string sourcePartitionKeyMapping = Environment.GetEnvironmentVariable($"{"SourcePartitionKeyMapping"}");
-        //string[] sourcePartitionKeyMaps = sourcePartitionKeyMapping.Split(',');
-
         public DocumentFeedMigrator(Container containerToStoreDocuments)
         {
             this.containerToStoreDocuments = containerToStoreDocuments;
@@ -44,6 +37,7 @@ namespace MigrationExecutorFunctionApp
             ConnectionStringSetting = "SourceCosmosDB",
             LeaseCollectionName = "leases",
             StartFromBeginning = true,
+            
             MaxItemsPerInvocation = 10000000,
             CreateLeaseCollectionIfNotExists = true)
             ]IReadOnlyList<Document> documents,
@@ -58,24 +52,18 @@ namespace MigrationExecutorFunctionApp
                 foreach (Document doc in documents)
                 {
                     dynamic json = JsonConvert.DeserializeObject(doc.ToString());
-                    var jsonReader = JsonReaderWriterFactory.CreateJsonReader(Encoding.UTF8.GetBytes(doc.ToString()), new System.Xml.XmlDictionaryReaderQuotas());
-                    var root = XElement.Load(jsonReader);
-                    string newValue = root.XPathSelectElement(sourcePartitionKeyMapping).Value;
-                    json[targetPartitionKeyAttribute] = newValue;
+                    json[targetPartitionKeyAttribute] = json[sourcePartitionKeyMapping];
                     Document document = new Document();
-                    JsonReader reader = new JTokenReader(json);                   
+                    JsonReader reader = new JTokenReader(json);
                     document.LoadFrom(reader);
                     tasks.Add(this.containerToStoreDocuments.CreateItemAsync(item: document, cancellationToken: cancellationToken).ContinueWith((Task<ItemResponse<Document>> task) =>
                     {
                         AggregateException innerExceptions = task.Exception.Flatten();
                         CosmosException cosmosException = (CosmosException)innerExceptions.InnerExceptions.FirstOrDefault(innerEx => innerEx is CosmosException);
-
                         failedMetrics.AddOrUpdate((int)cosmosException.StatusCode, 0, (key, value) => value + 1);
-
                         return postMortemQueue.AddAsync(document);
                     }, TaskContinuationOptions.OnlyOnFaulted));
                 }
-
                 await Task.WhenAll(tasks);
                 stopwatch.Stop();
 
